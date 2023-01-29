d5022488df482cb870fb8d8c37d874ecb052dc30
kind-kar <kind-kar@mail.ru>
2023-01-29 20:20:59 +0100

    git fix add files

---
+++ order-service/src/main/java/com/programming/orderservice/model/Order.java
@@ -0,0 +1,28 @@
+package com.programming.orderservice.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.springframework.data.annotation.Id;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Table;
+import java.util.List;
+
+@Entity
+@Table(name = "t_orders")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class Order {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+    private String orderNumber;
+    private List<OrderLineItems> orderLineItemsList;
+}
---
+++ order-service/src/main/java/com/programming/orderservice/model/OrderLineItems.java
@@ -0,0 +1,5 @@
+package com.programming.orderservice.model;
+
+public class OrderLineItems {
+
+}
